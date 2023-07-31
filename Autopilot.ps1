02603fea094d8cb9400e39d362bce1b37cd5ad52
mortenhavneraas <56255376+mortenhavneraas@users.noreply.github.com>
2023-07-31 10:12:52 +0200

    Delete Autopilot.ps1

--- Autopilot.ps1
+++
@@ -1,4 +0,0 @@
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Confirm:$false -Force:$true
-Install-Script get-windowsautopilotinfo -Confirm:$false -Force:$true
-get-windowsautopilotinfo -Online -TenantId "" -AppId "" -AppSecret ""
-shutdown.exe /r /t 60
